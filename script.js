2470abaef6ef15c2124b409ee071706985525356
chedj09 <cherryl@kaveblocks.com>
2022-05-02 10:05:06 +0800

    display the background for the JS games

--- script.js
+++ script.js
@@ -8,14 +8,39 @@ const cellSize = 100;
 const cellGap = 3;
 const gameGrid =[];
 
+// mouse
+
 
 //game board
 const controlsBar = {
   width: canvas.width,
   height: cellSize,
 }
-
-
+class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = cellSize;
+    this.height = cellSize;
+  }
+  draw() {
+    ctx.strokeStyle = 'pink';
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+  }
+}
+function createGrid(){
+  for(let y = cellSize; y < canvas.height; y += cellSize){
+    for (let x = 0; x<canvas.width; x +=cellSize){
+      gameGrid.push(new Cell(x, y));
+    }
+  }
+}
+createGrid();
+function handleGameGrid(){
+  for(let i =0; i <gameGrid.length; i++){
+    gameGrid[i].draw();
+  }
+}
 
 //projectiles
 //defenders
@@ -26,6 +51,7 @@ function animate(){
   // Not working
   ctx.fillStyle = 'pink';
   ctx.fillRect(0,0,controlsBar.width, controlsBar.height);
+  handleGameGrid();
   requestAnimationFrame(animate);
 }
 animate();
